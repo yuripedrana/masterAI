76a3cb5ffa6049d935fefd2f06fee28639287d86
yuri_c0d3r <ypedrana04@icloud.com>
2026-09-01 10:27:47 +0200

    VARIABILI E TIPI DI DATI

---
+++ tipi-di-dati.py
@@ -0,0 +1,21 @@
+x = 7
+y = 3
+
+somma = x + y
+print(somma)
+
+prezzo = 19.99
+quantita = 3
+
+totale = prezzo * quantita
+print(totale)
+
+var_num = 10
+print(var_num < 5)
+print(var_num > 5)
+
+a = 5
+b = 2.5
+
+somma = a + b
+print(somma)
---
+++ variabili.py
@@ -0,0 +1,9 @@
+mionome = "Yuri"
+print(mionome)
+
+anni = int(input("Quanti anni hai?"))
+print(anni * 2)
+
+n1 = 2
+n2 = 3
+print(n1 + n2)
