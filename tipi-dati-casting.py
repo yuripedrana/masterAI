4afe90219f7269ac78f463c3ef3fe9475ff3220f
yuri_c0d3r <ypedrana04@icloud.com>
2026-09-02 22:33:56 +0200

    TIPI DI DATI E CASTING

---
+++ tipi-dati-casting.py
@@ -0,0 +1,14 @@
+n_iniziale = 30
+print("Il valore iniziale di n è:", n_iniziale)
+
+n_iniziale_float = float(n_iniziale)
+print("Il valore di n come float è:", n_iniziale_float)
+
+n_iniziale_str = str(n_iniziale)
+print("Il valore di n come stringa è:", n_iniziale_str)
+
+n_iniziale_bool = bool(n_iniziale)
+print("Il valore di n come booleano è:", n_iniziale_bool)
+
+n_finale = n_iniziale * 3
+print("inizio:", n_iniziale, "finale:", n_finale)
